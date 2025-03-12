99991c91097f32b9d9c4e225ec56ce4ec4b78acc
RenanMarinho809 <renanmarinho809@gmail.com>
2025-03-12 18:39:52 -0300

    fix: correct typo in start script and add MongoDB connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import connectDB from './config/db.js';
 
 const app = express();
 
 const port = 3000;
+
+connectDB();
 
 app.listen(port, () => {console.log('App is running on port' + port)})
